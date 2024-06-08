1f9ec3c050acb28d86bf058a05bd32b75d095dc7
NehaYadav010 <yadavneha594@gmail.com>
2024-06-08 13:48:09 +0530

    elements structured

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const scroll = new LocomotiveScroll({
 });
 
 // Element Container
+
+var elemC = document.querySelector("#elem-container")
+var fixedc = document.querySelector("#fixed-image")
+elemC.addEventListener("mouseenter", ()=>{
+    fixedc.style.display = "block";
+})
+elemC.addEventListener("mouseleave", ()=>{
+    fixedc.style.display = "none";
+})
